6209366bbe879ff198635a6567bd9fffc1b2115c
LEONE DA SILVA MELO <leone.smelo@senacsp.edu.br>
2024-10-28 22:05:31 -0300

    Minidev

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log("Processo principal")
 // nativeTheme (forçar um tema no OS)
 // Menu (criar um menu personalizado)
 // shell (acessar links externos)
-const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
+const { app, BrowserWindow, nativeTheme, Menu, shell, ipcMain } = require('electron/main')
 const path = require('node:path')
 
 //janela principal
@@ -28,15 +28,35 @@ function createWindow() {
 //janela sobre (secudaria)
 function aboutWindow() {
   nativeTheme.themeSource = 'dark'
-  const about = new BrowserWindow({
-    width: 360,
-    height: 220,
-    autoHideMenuBar: true, //esconder o menu
-    resizable: false, //impedir redimensionamento
-    minimizable: false, // impedir minizae a janela
-    //titleBarStyle: 'hidden' //esconder a barra de estilo (ex: totem de auto atendimento)
-  })
+  //a linha abaixo obtem a jenela principal
+  const main = BrowserWindow.getFocusedWindow()
+  let about
+  // validar a janela pai
+  if (main){
+    about = new BrowserWindow({
+      width: 320,
+      height: 160,
+      autoHideMenuBar: true, //esconder o menu
+      resizable: false, //impedir redimensionamento
+      minimizable: false, // impedir minizae a janela
+      //titleBarStyle: 'hidden' //esconder a barra de estilo (ex: totem de auto atendimento)
+      parent: main, //estabelece uma hierarquia de janelas
+      modal: true, //ativa esse comportamento de janela modal
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+  }
   about.loadFile('./src/views/sobre.html')
+  
+  //fechar a janela quando receber mensagem do processo de renderização.
+  ipcMain.on('close-about', () => {
+      console.log("recebi a mensagem close-about")
+      //validar se a janela foi destruida
+      if(about && !about.isDestroyed()){
+        about.close()
+      }
+  })
 }
 
 
---
+++ src/views/render.js
@@ -0,0 +1,11 @@
+/**
+ * Processos de renderização
+ */
+
+console.log("Processo de renderização")
+
+// Botão OK (janela sobre)
+function fechar(){
+    api.fecharJanela()
+}
+
